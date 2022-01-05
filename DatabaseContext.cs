c0d5403554147bb3d45455bfcba454c3d7a2f1fa
Eszter Szabó <poszke0517@gmail.com>
2022-01-05 14:40:29 +0100

    DatabaseContext and model

---
+++ DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using CarDataRecognizer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarDataRecognizer
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Adat> Adatok { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Adat>(b =>
+            {
+                b.HasIndex(e => new { e.Date, e.PlateNumber }).IsUnique();
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Models/Adat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarDataRecognizer.Models
+{
+    public class Adat
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int KameraId { get; set; }
+
+        /// <summary>
+        /// When the picture is observed.
+        /// </summary>
+        public DateTime? Date { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        public string PlateNumber { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Color { get; set; }
+
+        /// <summary>
+        ///Vehicle type (p.es. bus vehicle).
+        /// </summary>
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Type { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Brand { get; set; }
+
+        /// <summary>
+        /// How accurate the provided data is.
+        /// </summary>
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Validity { get; set; }
+    }
+}
